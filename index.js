2fa7439c720b089e01755cb517b2c08b86163aa0
Flukus99 <elementary@outlook.es>
2020-01-20 01:42:12 -0500

    puliendo detalles ,  queda nada prolijo, sacando inormacion con click

--- index.js
+++ index.js
@@ -5,23 +5,44 @@ async function load(){
   
     for (let i = 1; i <=300; i++) {
         var personaje20=await llamarPersonaje(i);
-        
+        var escucha=await escuchar(i)
  }
 }
 async function llamarPersonaje(id){
     const data=await fetch("https://rickandmortyapi.com/api/character/".concat(id))
     const personaje= await data.json()
     const parametro=personaje.image
-
-    let template=`<div class="hover">
+    const nombre=personaje.name
+    const status=personaje.status
+    const especie=personaje.species
+    let template=`<div class="hover" id="${id}">
     <img src="${parametro}" alt="">
+    <div class="informacion">
+    <p>${nombre}</p>
     
+    </div>
     </div>`
  
     html.body.innerHTML=template;
     $personajes.append(html.body.children[0])
 }
 load()
+
+
+
+
+
+
+function escuchar(id){
+var imagen_html=document.getElementById(id);
+imagen_html.addEventListener("click",siFunciona);
+ async function siFunciona(){
+    const data=await fetch("https://rickandmortyapi.com/api/character/".concat(id));
+    const data_json= await data.json();
+    console.log(data_json)
+}
+
+}
 
 
 
